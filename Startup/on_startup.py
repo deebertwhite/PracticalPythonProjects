bf9aa53763f571ddb2e4500d7cb89a46e7ba38d3
deebertwhite <46319069+deebertwhite@users.noreply.github.com>
2022-12-28 09:44:02 -0600

    Add files via upload

---
+++ Startup/on_startup.py
@@ -0,0 +1,49 @@
+import os, shutil, winshell
+
+## TEMP FOLDER EMPTY PROCESS ##
+## SOME FILES WILL NOT BE DELETED ##
+
+getUser = os.getlogin()
+pathToUser = r'C:\Users'
+userPath = os.path.join(pathToUser, getUser)
+tempLocal = r'\AppData\Local\Temp'
+tempPathLocal = userPath + tempLocal
+
+#tempPathLocal = r'C:\Users\JAMES~1.W_U\AppData\Local\Temp'
+tempDeleteLocal = os.listdir(tempPathLocal)
+
+for file in tempDeleteLocal:
+    tempDeletePath = os.path.join(tempPathLocal, file)
+    try:
+        if os.path.isfile(tempDeletePath) or os.path.islink(tempDeletePath):
+            os.unlink(tempDeletePath)
+        elif os.path.isdir(tempDeletePath):
+            shutil.rmtree(tempDeletePath)
+    except Exception as e:
+        print('Failed to delete %s. Reason %s' % (tempDeletePath, e))
+
+## TEMP FOLDER EMPTY PROCESS ##
+## SOME FILES MAY NOT BE DELETED ##
+#tempPath = 'C:\\Windows\\Temp'
+
+tempPath = r'C:\Windows\Temp'
+tempDelete = os.listdir(tempPath)
+
+for file in tempDelete:
+    tempDeletePath = os.path.join(tempPath, file)
+    try:
+        if os.path.isfile(tempDeletePath) or os.path.islink(tempDeletePath):
+            os.unlink(tempDeletePath)
+        elif os.path.isdir(tempDeletePath):
+            shutil.rmtree(tempDeletePath)
+    except Exception as e:
+        print('Failed to delete %s. Reason %s' % (tempDeletePath, e))
+
+## EMPTY RECYCLE BIN IF FULL ##
+
+isBinEmpty = list(winshell.recycle_bin())
+
+if isBinEmpty == []:
+    print('Nothing to delete')
+else:
+    winshell.recycle_bin().empty(confirm= False)
